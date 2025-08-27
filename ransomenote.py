b97138f6a58abef3be4bac291db1f56e944d76d0
Mr. Coder88 <alimohammadw1967@gmail.com>
2025-08-27 23:16:33 +0400

    Add files via upload

---
+++ ransomenote.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+magazine = input("Enter the magazine text: ")
+notebook = input("Enter the ransom note text: ")
+
+mag, note = Counter(magazine), Counter(notebook)
+
+if mag & note == note:
+    print("you have enough letters")
+else:
+    print("you do not have enough letters")
